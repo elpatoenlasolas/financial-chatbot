77a00d06e89d5a62eb44cf33d1b0eb12a0f828b9
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-09-24 18:23:26 -0300

    Update sha256

--- financial-chatbot.rb
+++ financial-chatbot.rb
@@ -4,7 +4,7 @@ class FinancialChatbot < Formula
     desc "A terminal bot for financial literacy"
     homepage "https://github.com/elpatoenlasolas/financial-chatbot"
     url "https://github.com/elpatoenlasolas/financial-chatbot/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" 
+    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" 
     license "MIT"
   
     depends_on "python@3.11"
