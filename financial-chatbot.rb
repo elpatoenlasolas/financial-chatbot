51cf072e3229128d97ee60b366dd6eeb1abcaa16
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-09-24 18:18:18 -0300

    Update financial-chatbot.rb

--- financial-chatbot.rb
+++ financial-chatbot.rb
@@ -7,7 +7,7 @@ class FinancialChatbot < Formula
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" 
     license "MIT"
   
-    depends_on "python@3.11.3"
+    depends_on "python@3.11"
   
     def install
       virtualenv_install_with_resources
