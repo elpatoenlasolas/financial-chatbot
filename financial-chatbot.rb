d6305dbd5998e94b41ad80860d24a92941bcc46b
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-09-24 18:15:41 -0300

    Add files via upload

---
+++ financial-chatbot.rb
@@ -0,0 +1,19 @@
+class FinancialChatbot < Formula
+    include Language::Python::Virtualenv
+  
+    desc "A terminal bot for financial literacy"
+    homepage "https://github.com/elpatoenlasolas/financial-chatbot"
+    url "https://github.com/elpatoenlasolas/financial-chatbot/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" 
+    license "MIT"
+  
+    depends_on "python@3.11.3"
+  
+    def install
+      virtualenv_install_with_resources
+    end
+  
+    test do
+      system "#{bin}/financial-chatbot", "--help" 
+    end
+  end  
