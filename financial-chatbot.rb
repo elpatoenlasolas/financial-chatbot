50a1ab4d3ae9cc3a90f014deef2fafd2e3572ac7
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-09-24 19:11:56 -0300

    Update financial-chatbot.rb

--- financial-chatbot.rb
+++ financial-chatbot.rb
@@ -3,7 +3,7 @@ class FinancialChatbot < Formula
   
     desc "A terminal bot for financial literacy"
     homepage "https://github.com/elpatoenlasolas/homebrew-financial-bot"
-    url "https://github.com/elpatoenlasolas/homebrew-financial-bot/archive/refs/tags/v0.1.0.tar.gz"
+    url "https://github.com/elpatoenlasolas/homebrew-financial-bot/archive/refs/tags/v0.1.1.tar.gz"
     sha256 "84560c444ed4ebb89e8db92677a55414f39d0cc2c9cfc5919bf5f30146c8454d" 
     license "MIT"
   
