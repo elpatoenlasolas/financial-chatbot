3ad043e6a2c62e918c7c034bad9fcb6ee70a9fdb
patz <patofunes@gmail.com>
2024-09-25 14:03:01 +0300

    updated tar

--- Formula/financial-chatbot.rb
+++ Formula/financial-chatbot.rb
@@ -4,9 +4,6 @@ class FinancialChatbot < Formula
     desc "A terminal bot for financial literacy"
     homepage "https://github.com/elpatoenlasolas/homebrew-financial-chatbot"
     url "https://github.com/elpatoenlasolas/homebrew-financial-chatbot/archive/refs/tags/0.1.0.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" 
-    url "https://github.com/elpatoenlasolas/homebrew-financial-chatbot/archive/refs/tags/v0.1.0.tar.gz"
-    url "https://github.com/elpatoenlasolas/homebrew-financial-chatbot/archive/refs/tags/0.1.0.tar.gz"
     sha256 "0c8a75e1db8d20ab355f1d43aed531e5562665b53b64cb03beba9ac8021b3fa4f" 
     license "MIT"
   
